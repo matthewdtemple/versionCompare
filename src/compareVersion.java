5580b06ab3541f3b14cbcb42881b864112c1dadd
Matthew Temple <mattdtemple@gmail.com>
2022-11-05 15:52:25 -0500

    comparing seems to be working correctly

--- src/compareVersion.java
+++ src/compareVersion.java
@@ -1,16 +1,17 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class compareVersion {
 
-    public static int versionComparison(String v1, String v2){
+    public static int versionComparison(String v1, String v2) {
 
         // split arguments by . into array
         String[] splitv1 = v1.split("\\.");
         String[] splitv2 = v2.split("\\.");
 
         List<Integer> intv1 = new ArrayList<>();
-        List <Integer> intv2 = new ArrayList<>();
+        List<Integer> intv2 = new ArrayList<>();
 
         // add Integer values into new int array
         for (int i = 0; i < splitv1.length; i++) {
@@ -23,32 +24,40 @@ public class compareVersion {
 
         // if sizes are not the same, add to the smaller array the difference
         int sizeDifference = 0;
-        if (intv1.size() < intv2.size()){
+        if (intv1.size() < intv2.size()) {
             sizeDifference = intv2.size() - intv1.size();
-            for (int i = 0; i < sizeDifference; i++){
+            for (int i = 0; i < sizeDifference; i++) {
                 intv1.add(0);
             }
-        } else if (intv1.size() > intv2.size()){
+        } else if (intv1.size() > intv2.size()) {
             sizeDifference = intv1.size() - intv2.size();
-            for (int i = 0; i < sizeDifference; i++){
+            for (int i = 0; i < sizeDifference; i++) {
                 intv2.add(0);
             }
             // compare integers and return appropriate value
         }
+
+
         int comparedResult = 25; //test to see if logic changes this value
-        for (int i = 0; i < intv1.size(); i++){
-            if (intv1.get(i) > intv2.get(i) ){
-                comparedResult = 1;
-            } else if (intv1.get(i) < intv2.get(i)) {
-                comparedResult = -1;
-            } else {
-                comparedResult = 0;
+            for (int i = 0; i < intv1.size(); i++) {
+                if (Arrays.equals(intv1.toArray(),intv2.toArray())) {
+                    comparedResult = 0;
+                    break;
+                } while (intv1.get(i) != intv2.get(i)) {
+                    if (intv1.get(i) > intv2.get(i)) {
+                        comparedResult =1;
+                        break;
+                    } else if (intv1.get(i) < intv2.get(i)) {
+                        comparedResult = -1;
+                        break;
+                    }
+                }
             }
-        }
-        return comparedResult;
+            return comparedResult;
+
     }
 
-    public static void main (String[] args){
-        System.out.println(versionComparison("1.1.4", "1.1.0.0.1"));
+    public static void main (String[]args){
+        System.out.println(versionComparison("3.0.1.9", "3.0.1.0.0.0"));
     }
 }
